4596b5b42507eca5e3a3ec0847d09ed6d078296c
pavelhym <pavelhym@gmail.com>
2021-10-07 23:15:30 +0300

    Almost done

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -10,24 +10,33 @@ import scipy.stats as st
 from scipy.optimize import minimize
 import scipy.stats as stats
 import pylab 
+from scipy.stats import norm
+from scipy.stats import kde
+from scipy.stats import gamma
+import scipy
+from scipy import stats
 
 
 df = pd.read_csv("SUPERLAST.csv")
 
+df = df[["date", "Adj Close",'gtrends','Comments_int','tweet_num','meme_Reddit']]
 
 #Step 2
 
-#for i in df.columns[2:12]:
-#    sns.kdeplot(df[str(i)])
-#    plt.hist(df[str(i)], density=True)
-#    plt.title(str(i))
-#    plt.show()  
 
-for i in df.columns[2:12]:
-    fig, ax = plt.subplots()
-    sns.distplot(df[str(i)], bins=25, color="g", ax=ax)
-    plt.title(str(i))
+
+for i in df.columns[1:]:
+    x = df[str(i)]
+    density = kde.gaussian_kde(x)
+    xgrid = np.linspace(x.min(), x.max(), 100)
+    plt.hist(x, bins=8,density=True, stacked=True)
+    plt.plot(xgrid, density(xgrid), 'r-')
     plt.show()
+
+
+
+#Analog
+#sns.distplot(df.gtrends, kde=True, norm_hist=True)
 
 
 #Step 3
@@ -79,31 +88,38 @@ def getLL_normal(params, data):
 
 
 guess = np.array([1,3])
-results = minimize(getLL_normal, [1,3], args = (df.gtrends))
+results_ML = minimize(getLL_normal, [1,3], args = (df.gtrends))
 
-mu_est, sigma_est = results.x
+mu_est_ML, sigma_est_ML = results_ML.x
+
 #estimating with OLS
-
-
-
-np.quantile(df['Adj Close'], 1)
-
-# OLS
 
 def OLS(params, data):
     mu,sigma = params
     quantiles = [0.15,0.25,0.35,0.45,0.5,0.65,0.75,0.85,0.95]
-    s = np.random.normal(mu, sigma, 1000)
+    #s = np.random.normal(mu, sigma, 1000)
     err = 0
     for i in quantiles:
-        err += (np.quantile(data, i) - np.quantile(s, i))**2
-
+        err += (np.quantile(data, i) - norm.ppf(i, loc=mu, scale=sigma))**2
     return err
 
-OLS([12,13],df.gtrends)
 
-guess = np.array([1,3])
-results = minimize(OLS, [12,13], args = (df.gtrends),method = 'Powell')
+
+
+results_OLS = minimize(OLS, [12,13], args = (df.gtrends),method = 'Powell')
+mu_est_OLS, sigma_est_OLS = results_OLS.x
+
+#with standart func
+norm.fit(df.gtrends)
+
+
+x = np.arange(min(df.gtrends), max(df.gtrends), 1)
+plt.plot(x, norm.pdf(x, mu_est_OLS, sigma_est_OLS),label="OLS")
+plt.plot(x, norm.pdf(x, mu_est_ML, sigma_est_ML),label = "ML")
+plt.hist(df.gtrends, density=True)
+plt.legend()
+
+
 
 
 
@@ -118,4 +134,10 @@ pylab.show()
 #kolmogorov test
 stats.kstest(df.gtrends, np.random.normal(mu_est, sigma_est, 1000)  ,alternative='two-sided', mode='auto')
 
-#Omega squared test
+#Chi-Squared
+
+chi2 = scipy.stats.chisquare(df.gtrends)
+
+#Wilcoxon rank-sum
+scipy.stats.ranksums(df.gtrends, np.random.normal(13, 12, 1000))
+
